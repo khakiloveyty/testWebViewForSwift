678ed5d6d6e8598c2c2b9c47749487892e766bda
包金 <1426756048@qq.com>
2015-12-24 07:53:34 +0800

    Initial Commit

---
+++ testWebViewForSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testWebViewForSwift
+//
+//  Created by 包金 on 15/12/24.
+//  Copyright © 2015年 包金. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
